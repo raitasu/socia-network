1a1f59eea3141c025e8a0806be1f7d95ba58d2b1
raitasu <pavelburyak3@gmail.com>
2022-06-17 18:42:40 +0300

    feat: added form login

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import classes from './Login.module.css';
+import * as Yup from 'yup'
+
+const loginFormSchema = Yup.object({
+    email: Yup.string().email('Invalid e-mail').required('Required'),
+    password: Yup.string().min(8, 'Must be longer than 8 characters').required('Required'),
+});
 
 const Login = () => {
     return (
-        <div>
-            <h1>LOGIN</h1>
+        <div className={classes.login}>
+            <h1>Login</h1>
+            <Formik
+                initialValues={{email: '', password: '', rememberMe: false}}
+                validationSchema={loginFormSchema}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {() => (
+                    <Form>
+                        <div>
+                            <Field type='email' name='email' placeholder='e-mail'/>
+                            <ErrorMessage name='email' component='p'/>
+                        </div>
+                        <div>
+                            <Field type='password' name='password' placeholder='password'/>
+                            <ErrorMessage name='password' component='p'/>
+                        </div>
+                        <div>
+                            <Field type='checkbox' name='rememberMe'/>
+                            <label htmlFor='rememberMe'>remember me</label>
+                        </div>
+                        <button type='submit'>Log in</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 };
